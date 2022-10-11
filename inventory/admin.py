bee92ee6143ed2c68062f46a8bd14e5aa35704f5
nasir-gaayte <slowboyz12@gmail.com>
2022-10-11 16:49:45 +0300

    admin to model in one and prepopulated slug with name

--- inventory/admin.py
+++ inventory/admin.py
@@ -4,10 +4,47 @@ from .models import Product, Category, StockControl,Image, Inventory,AttributeVa
 # Register your models here.
 
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Attribute)
-admin.site.register(AttributeValue)
-admin.site.register(Inventory)
-admin.site.register(Image)
-admin.site.register(StockControl)
+# admin.site.register(Category)
+# admin.site.register(Product)
+# admin.site.register(Attribute)
+# admin.site.register(AttributeValue)
+# admin.site.register(Inventory)
+# admin.site.register(Image)
+# admin.site.register(StockControl)
+
+class ProductImageInline(admin.TabularInline):
+    model = Image
+
+
+class StockControlInline(admin.TabularInline):
+    model = StockControl    
+
+class AttributeValueInline(admin.TabularInline):
+    model = AttributeValue
+
+
+
+
+
+
+@admin.register(Inventory)
+class ProductInventoryAdmin(admin.ModelAdmin):
+    inlines= [ProductImageInline, StockControlInline]   
+
+@admin.register(Attribute)
+class ProductAdmin(admin.ModelAdmin):
+    inlines= [AttributeValueInline,]   
+
+  
+@admin.register(Category)
+class GategoryAdmin(admin.ModelAdmin):
+        prepopulated_fields = {
+            "slug":("name",),
+        }
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+        prepopulated_fields = {
+            "slug":("name",),
+        }        
